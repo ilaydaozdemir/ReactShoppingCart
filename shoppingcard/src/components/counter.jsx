0f63b75855fe14515a50b4bb7b25bb3b97e191d2
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-11-28 22:00:01 +0300

    Add Reset Button and Delete Button

--- shoppingcard/src/components/counter.jsx
+++ shoppingcard/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleIncrement = () => {
@@ -10,16 +10,20 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
-        <h4>{this.props.id}</h4>
         <span className={this.getBadge()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className='btn btn-secondary btn-sm'
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className='btn btn-danger btn-sm m-2'
+        >
+          Delete
         </button>
       </div>
     );
--- shoppingcard/src/components/counters.jsx
+++ shoppingcard/src/components/counters.jsx
@@ -11,13 +11,34 @@ class Counters extends Component {
     ],
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className='btn btn-primary btn-sm m-2'
+        >
+          Reset
+        </button>
+
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} id={counter.id}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          ></Counter>
         ))}
       </div>
     );
